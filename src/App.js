dd94bba80b16c2303bca01a8adcaa5784e1046e4
Brian Wang <brianwang17@yahoo.com>
2021-03-10 16:00:19 -0800

    Adds in progress widgets application; still needs routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import React, { useState } from 'react';
 import "./styles.css";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -17,30 +22,42 @@ const items = [
   }
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red"
+  },
+  {
+    label: "The Color Green",
+    value: "green"
+  },
+  {
+    label: "The Color Blue",
+    value: "blue"
+  }
+]
+
 export default () => {
+
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      {/* <Accordion items={items} /> */}
-      <Search />
+    <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown label="Select a color" options={options} selected={selected} onSelectedChange={setSelected} />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
 
-// useEffect(() => {
-//   const search = async () => {
-//     const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-//       params: {
-//         action: 'query',
-//         list: 'search',
-//         origin: '*',
-//         format: 'json',
-//         srsearch: debouncedTerm,
-//       },
-//     });
 
-//     setResults(data.query.search);
-//   };
-//   if (debouncedTerm) {
-//     search();
-//   }
-// }, [debouncedTerm]);
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,49 +3,52 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  console.log("result: ", results);
+  
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
-      const { data } = await axios.get("https://en.wikipedia.org/w/api/php", {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term
+          srsearch: debouncedTerm
         }
       });
 
       setResults(data.query.search);
-
-      if (term && !results.length) {
-        search();
-      } else {
-        const timeoutId = setTimeout(() => {
-          if (term) search();
-        }, 1000);
-
-        return () => {
-          clearTimeout(timeoutId);
-        };
-      }
     };
-  }, [term, results.length]);
+
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
-      <div key={result.pageid} className="item">
+      <div className="item" key={result.pageid}>
         <div className="right floated content">
           <a
-            className="ui button"
             href={`https://en.wikipedia.org?curid=${result.pageid}`}
-          >
-            Go
+            className="ui button">Go
           </a>
         </div>
         <div className="content">
-          <div className="header">{result.title}</div>
+          <div className="header">
+            {result.title}
+          </div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
@@ -56,15 +59,16 @@ const Search = () => {
     <div>
       <div className="ui form">
         <div className="field">
-          <label>Enter Search Term</label>
+          <label htmlFor="">Enter Search Term</label>
           <input
-            value={term}
-            onChange={(e) => setTerm(e.target.value)}
+            type="text"
             className="input"
+            value={term}
+            onChange={e => setTerm(e.target.value)}
           />
         </div>
       </div>
-      <div className="ui celled list"></div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
